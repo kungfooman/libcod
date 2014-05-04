c1f1cb2404f914d1ad23f31c5149f96efd759d8f
iznogod <iznogod@users.noreply.github.com>
2014-05-05 00:46:03 +0200

    Add 0.1 alpha translation helper functions

--- gsc_utils.cpp
+++ gsc_utils.cpp
@@ -5,10 +5,340 @@
 #include <dirent.h> // dir stuff
 #include <assert.h>
 
+#define MAX_LANGUAGES 16
+#define MAX_LANGUAGE_ITEMS 1024
+
 // 1.2 0x080F6D5A
 int utils_hook_player_eject(int player) { // player 0 = 0x08679380 + 0x11c = 0x0867949c
 	//printf("int hook_player_eject(int player=%.8x)\n", player);
 	return 0;
+}
+
+int languages_defined = 0;
+int language_items_defined = 0;
+char languages[MAX_LANGUAGES][3]; //add space for \0
+char *language_items[MAX_LANGUAGE_ITEMS];
+char *language_references[MAX_LANGUAGES][MAX_LANGUAGE_ITEMS];
+bool language_reference_mallocd[MAX_LANGUAGES][MAX_LANGUAGE_ITEMS];
+
+void gsc_add_language()
+{
+	char *str;
+	if(!stackGetParamString(0, &str))
+	{
+		printf("Param 0 needs to be a string for add_language\n");
+		stackPushUndefined();
+		return;
+	}
+	if(str[0] == '\0' || str[1] == '\0' || str[2] != '\0')
+	{
+		printf("Languages are defined by 2 characters\n");
+		stackPushUndefined();
+		return;
+	}
+	for(int i = 0; i < languages_defined; i++)
+	{
+		if(!strcmp(languages[i], str))
+		{
+			printf("%s is already an added language\n", str);
+			stackPushUndefined();
+			return;
+		}
+	}
+	if(languages_defined == MAX_LANGUAGES)
+	{
+		printf("Cannot add another language. Already got %d languages\n", MAX_LANGUAGES);
+		stackPushUndefined();
+		return;
+	}
+	strcpy(languages[languages_defined], str);
+	languages_defined++;
+	printf("Added %s as language %d\n", str, languages_defined);
+	stackPushInt(0);
+}
+
+void add_lang_item(char* lang, char* item, char* txt)
+{
+	int language_number = -1;
+	for(int i = 0; i < languages_defined; i++)
+	{
+		if(languages[i][0] == lang[0] && languages[i][1] == lang[1])
+		{
+			language_number = i;
+			break;
+		}
+	}
+	if(language_number == -1)
+	{
+		printf("Language (%s) not added\n", lang);
+		return;
+	}
+	int language_item_number = language_items_defined;
+	for(int i = 0; i < language_items_defined; i++)
+	{
+		if(!strcmp(language_items[i], item))
+		{
+			language_item_number = i;
+			break;
+		}
+	}
+	if(language_item_number == MAX_LANGUAGE_ITEMS)
+	{
+		printf("Maximum language items reached\n");
+		return;
+	}
+	bool fill_other_langs = false;
+	if(language_item_number == language_items_defined)
+	{
+		char *item_m = (char*)malloc(sizeof(char) * (COD2_MAX_STRINGLENGTH + 1));
+		if(item_m == NULL)
+		{
+			printf("Could not malloc\n");
+			return;
+		}
+		fill_other_langs = true;
+		strncpy(item_m, item, COD2_MAX_STRINGLENGTH);
+		language_items[language_item_number] = item_m;
+		language_items_defined++;
+		for(int i = 0; i < languages_defined; i++)
+			language_reference_mallocd[i][language_item_number] = false;
+	}
+	char *txt_m;
+	if(!language_reference_mallocd[language_number][language_item_number])
+	{
+		txt_m = (char*)malloc(sizeof(char) * (COD2_MAX_STRINGLENGTH + 1));
+		if(txt_m == NULL)
+		{
+			printf("Could not malloc\n");
+			return;
+		}
+		language_reference_mallocd[language_number][language_item_number] = true;
+		language_references[language_number][language_item_number] = txt_m;
+	}
+	else
+		txt_m = language_references[language_number][language_item_number];
+	strncpy(txt_m, txt, COD2_MAX_STRINGLENGTH);
+	if(fill_other_langs)
+	{
+		for(int i = 0; i < languages_defined; i++)
+		{
+			if(i == language_number)
+				continue;
+			language_references[i][language_item_number] = txt_m;
+		}
+	}
+}
+
+void gsc_load_languages()
+{
+	static bool loaded = false;
+	char *str;
+	if(!stackGetParamString(0, &str))
+	{
+		printf("Param 0 needs to be a string for load_languages\n");
+		stackPushUndefined();
+		return;
+	}
+
+	int force_reload;
+	if(!stackGetParamInt(1, &force_reload))
+		force_reload = 0;
+	if(!force_reload && loaded)
+	{
+		printf("Already loaded languages\n");
+		stackPushUndefined();
+		return;
+	}
+	char curitem[COD2_MAX_STRINGLENGTH + 1] = "";
+	char buffer[COD2_MAX_STRINGLENGTH + 1];
+	bool item_found = false;
+	FILE * file;
+	file = fopen(str, "r");
+	int linenum = 0;
+	if(file != NULL)
+	{
+		while(fgets(buffer, sizeof(buffer), file) != NULL)
+		{
+			linenum++;
+			if(!strncmp(buffer, "REFERENCE", 9))
+			{
+				//read the rest of buffer, starting from the first non-space character
+				int start = -1;
+				int end = -1;
+				for(int i = 9; i < COD2_MAX_STRINGLENGTH; i++)
+				{
+					if(buffer[i] == '\0' || buffer[i] == '\r' || buffer[i] == '\n')
+					{
+						end = i;
+						if(end - start > 0)
+						{
+							//string has a length
+							//set it as curitem
+							strncpy(curitem, &(buffer[start]), end - start);
+							curitem[end - start] = '\0';
+							//printf("Read item: %s", curitem);
+							item_found = true;
+						}
+						break;
+					}
+					else if(start == -1 && buffer[i] != ' ' && buffer[i] != '\t')
+						start = i;
+					else if(start != -1 && (buffer[i] == ' ' || buffer[i] == '\t'))
+					{
+						//error, trailing whitespace
+						//try to cut it off
+						end = i;
+						if(end - start > 0)
+						{
+							//string has a length
+							//set it as curitem
+							strncpy(curitem, &(buffer[start]), end - start);
+							curitem[end - start] = '\0';
+							//printf("Read item: %s", curitem);
+							item_found = true;
+						}
+						break;
+					}
+				}
+			}
+			else if(!strncmp(buffer, "LANG_", 5))
+			{
+				//language is the [5] and [6]th element of this string
+				//rest of string, starting at the first " is the string, ending at the last "
+				bool lang_exist = false;
+				for(int i = 0; i < languages_defined; i++)
+				{
+					if(languages[i][0] == buffer[5] && buffer[5] != '\0' && languages[i][1] == buffer[6] && buffer[6] != '\0')
+					{
+						lang_exist = true;
+						break;
+					}
+				}
+				if(!lang_exist)
+				{
+					if(buffer[5] != '\0' && buffer[6] != '\0')
+						printf("Language not yet added for language: %c%c\n", buffer[5], buffer[6]);
+					else
+						printf("Line ended too soon on line %d\n", linenum);
+				}
+				else
+				{
+					//start scanning buffer, starting from 8
+					char lang[2];
+					lang[0] = buffer[5];
+					lang[1] = buffer[6];
+					int start = -1;
+					int end = -1;
+					bool ignore_next = false;
+					for(int i = 8; i < COD2_MAX_STRINGLENGTH; i++)
+					{
+						if(buffer[i] == '\\' && !ignore_next)
+						{
+							ignore_next = true;
+							continue;
+						}
+						if(buffer[i] == '\0' || buffer[i] == '\r' || buffer[i] == '\n')
+						{
+							//string ended prematurely
+							printf("Error in line %d\n", linenum);
+							break;
+						}
+						if(buffer[i] == '\"' && !ignore_next)
+						{
+							if(start == -1)
+							{
+								if(buffer[i + 1] == '\0')
+								{
+									printf("Premature line end on line %d", linenum);
+									break;
+								}
+								else
+									start = i + 1;
+							}
+							else
+							{
+								end = i;
+								//add buffer to languages stuff
+								if(end - start > 0)
+								{
+									char curdesc[COD2_MAX_STRINGLENGTH + 1];
+									strncpy(curdesc, &(buffer[start]), end - start);
+									curdesc[end - start] = '\0';
+									add_lang_item(lang, curitem, curdesc);
+									//printf("Adding %s as %s for language %c%c\n", curdesc, curitem, buffer[5], buffer[6]);
+								}
+								break;
+							}
+						}
+						ignore_next = false;
+					}
+				}
+			}
+		}
+		fclose(file);
+	}
+	else
+	{
+		printf("File %s does not exist\n", str);
+		return;
+	}
+}
+
+void gsc_get_language_item()
+{
+	char *str;
+	char *str2;
+	if(!stackGetParamString(0, &str))
+	{
+		printf("Param 0 has to be a string for get_language_item\n");
+		stackPushUndefined();
+		return;
+	}
+	if(!stackGetParamString(1, &str2))
+	{
+		printf("Param 1 has to be a string for get_language_item\n");
+		stackPushUndefined();
+		return;
+	}
+	if(str[0] == '\0' || str[1] == '\0' || str[3] != '\0')
+	{
+		printf("Invalid language item requested. Should be like EN_HELLO_WORLD\n");
+		stackPushUndefined();
+		return;
+	}
+	int language_number = -1;
+	for(int i = 0; i < languages_defined; i++)
+	{
+		if(str[0] == languages[i][0] && str[1] == languages[i][1])
+		{
+			//found a match
+			language_number = i;
+			break;
+		}
+	}
+	if(language_number == -1)
+	{
+		printf("Invalid language selected. Load languages first\n");
+		stackPushUndefined();
+		return;
+	}
+	int language_item_number = -1;
+	for(int i = 0; i < language_items_defined; i++)
+	{
+		if(!strcmp(str2, language_items[i]))
+		{
+			//found match
+			language_item_number = i;
+			break;
+		}
+	}
+	if(language_item_number == -1)
+	{
+		//printf("Invalid language item selected. Load language items first\n");
+		stackPushString(str2);
+		return;
+	}
+	stackPushString(language_references[language_number][language_item_number]);
 }
 
 void gsc_utils_sprintf()
